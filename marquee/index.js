1e90e7d1e11ff3731e22d9f021a7edb3195f2dab
Justin Hsu <b12031106@gmail.com>
2022-03-17 01:36:29 +0800

    feat: toolize marquee emoji geneator

--- marquee/index.js
+++ marquee/index.js
@@ -2,24 +2,26 @@ const body = document.body;
 const bodyStyle = getComputedStyle(body);
 const drawingZoneEl = document.getElementById('drawing-zone');
 const resultZoneEl = document.getElementById('result-zone');
+const generateButtonEl = document.getElementById('generate-btn');
+const textBodyEl = document.getElementById('text-body');
+const fpsEl = document.getElementById('fps');
+const animationDurationEl = document.getElementById('animation-duration');
+const emojiCountsEl = document.getElementById('emoji-counts');
 
-const GIF_CHUNKS_COUNT = 4;
+const inputs = [
+    textBodyEl,
+    fpsEl,
+    animationDurationEl,
+    emojiCountsEl,
+    generateButtonEl,
+];
+
 const SQUARE_SIZE = 64;
 const CANVAS_HEIGHT = SQUARE_SIZE;
-const CANVAS_WIDTH = GIF_CHUNKS_COUNT * SQUARE_SIZE;
 
 const FONT_COLOR = '#000';
 const BACKGROUND_COLOR = '#fff';
 
-// 禎數
-const FPS = 30;
-// 整個動畫長度
-const ANIMATION_DURATION = 5000;
-
-const FRAME_DURATION = 1000 / FPS;
-const TOTAL_FRAMES = ANIMATION_DURATION / FRAME_DURATION;
-
-const TEXT = '自卑與自信都是自己給自己的';
 const FONT_SIZE = '60px';
 const FONT_FAMILY = bodyStyle.fontFamily;
 
@@ -50,17 +52,17 @@ const clearCanvas = () => {
     })
 }
 
-const drawText = (posX) => {
+const drawText = (posX, text) => {
     clearCanvas();
     canvases.forEach((canvas, idx) => {
         let shiftPos = idx * SQUARE_SIZE;
         // 繪製畫面到 canvas
         canvas.ctx.fillStyle = FONT_COLOR;
-        canvas.ctx.fillText(TEXT, posX - shiftPos, SQUARE_SIZE / 2);
+        canvas.ctx.fillText(text, posX - shiftPos, SQUARE_SIZE / 2);
         // 將 canvas 的畫面加入 gif 的一個 frame
         canvas.gif.addFrame(canvas.ctx, {
             copy: true,
-            delay: FRAME_DURATION,
+            delay: getFrameDuration(),
         });
     });
 }
@@ -85,43 +87,47 @@ const generateFileDownloadLink = (canvas, idx) => {
     canvas.gif.render();
 };
 
-const startAnimation = () => {
+const startAnimation = (text) => {
     // 起點，canvas 的總寬度
-    let startPositionX = CANVAS_WIDTH;
+    let startPositionX = getCanvasWidth();
     // 終點，文字總長度 * -1，拿第一個 canvas dom 來算就好
-    let endPositionX = getMeasureTextWidth(canvases[0].ctx, TEXT) * -1;
+    let endPositionX = getMeasureTextWidth(canvases[0].ctx, text) * -1;
     let positionX = startPositionX;
 
     // 移動總距離
     const moveDistance = startPositionX - endPositionX;
     // 每 frame 移動的距離
-    const movePerFrame = Math.round(moveDistance / TOTAL_FRAMES);
+    const movePerFrame = Math.round(moveDistance / getTotalFrames());
 
     console.log(startPositionX, endPositionX);
     let interval = rafSetInterval(() => {
-        drawText(positionX);
+        drawText(positionX, text);
         if (positionX <= endPositionX) {
             console.log('end');
             interval.stop();
 
             resultZoneEl.innerHTML = '';
-            for (let i = 0; i < GIF_CHUNKS_COUNT; i += 1) {
+            for (let i = 0; i < getEmojiCounts(); i += 1) {
                 let container = document.createElement('a');
                 container.id = `result-${i}`;
                 resultZoneEl.appendChild(container);
                 generateFileDownloadLink(canvases[i], i);
             }
+            unlockAll();
             return;
         }
         positionX -= movePerFrame;
-    }, FRAME_DURATION);
+    }, getFrameDuration());
 }
 
-const initialize = () => {
-
+const doGenerate = (text) => {
+    if (!text) {
+        alert('請輸入文字');
+        return;
+    }
     // 建立足夠數量的 canvas element
     let fragment = document.createDocumentFragment();
-    for (let idx = 0; idx < GIF_CHUNKS_COUNT; idx += 1) {
+    for (let idx = 0; idx < getEmojiCounts(); idx += 1) {
         let canvasEl = createCanvas(idx, SQUARE_SIZE);
         fragment.appendChild(canvasEl);
         canvases.push({
@@ -139,7 +145,37 @@ const initialize = () => {
     }
     drawingZoneEl.appendChild(fragment);
 
-    startAnimation();
+    startAnimation(text);
 }
 
-initialize();
+const reset = () => {
+    // 清空作畫區跟結果區
+    drawingZoneEl.innerHTML = '';
+    resultZoneEl.innerHTML = '';
+    canvases = [];
+}
+
+const lockAll = () => {
+    inputs.map(elem => elem.disabled = true);
+}
+
+const unlockAll = () => {
+    inputs.map(elem => elem.disabled = false);
+}
+
+const getTextBody = () => textBodyEl.value.trim();
+const getFps = () => fpsEl.value;
+const getAnimationDuration = () => animationDurationEl.value;
+const getEmojiCounts = () => emojiCountsEl.value;
+const getCanvasWidth = () => getEmojiCounts() * SQUARE_SIZE;
+const getFrameDuration = () => 1000 / getFps();
+const getTotalFrames = () => getAnimationDuration() / getFrameDuration();
+
+// bind event
+generateButtonEl.addEventListener('click', () => {
+    reset();
+    lockAll();
+    doGenerate(getTextBody());
+});
+
+// initialize();
